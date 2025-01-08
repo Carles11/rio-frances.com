b7d683541b2499523ede787e51a9ed48c2235fb1
carlos <c.delriofrances@gmail.com>
2025-01-08 16:31:10 -0300

    home page hero section done

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,9 +1,82 @@
+'use client'
 import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
 
-type Props = {}
-
-const HeroSection = (props: Props) => {
-  return <h1 className="text-white"> Hello, I´m Carles</h1>
+const HeroSection = () => {
+  return (
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="min-h-[120px] lg:min-h-[450px] text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-white bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&apos;m{' '}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                'Carles',
+                1000,
+                'a Web Developer',
+                1000,
+                'a mobile App Developer',
+                1000,
+                'also a UI/UX Designer',
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Let´s get in touch. I´m available for freelance, full-time,
+            part-time, contract, and consulting opportunities.
+          </p>
+          <div>
+            <Link
+              href="/#contact"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              Hire Me
+            </Link>
+            <a
+              href="/files/Del_Rio_2025_WebDevCV_ENG.pdf"
+              download="Del_Rio_2025_WebDevCV_ENG.pdf"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+            >
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </span>
+            </a>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/carles-in-ids.png"
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  )
 }
 
 export default HeroSection
