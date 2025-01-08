ab06ecee2ef6295c4feb5498b00412338906a652
carlos <c.delriofrances@gmail.com>
2025-01-08 16:54:52 -0300

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import HeroSection from '@/app/components/HeroSection'
+import Navbar from '@/app/components/Navbar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]  ">
+      <Navbar />
       <div className="container mx-auto px-12 py-4">
         <HeroSection />
       </div>
