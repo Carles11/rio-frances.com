3ffe662880093595a04367913112c62e318cd4c4
carlos <c.delriofrances@gmail.com>
2025-01-08 17:41:06 -0300

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/app/components/HeroSection'
+import AboutSection from '@/app/components/AboutSection'
 import Navbar from '@/app/components/Navbar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]  ">
       <Navbar />
-      <div className="container mx-auto px-12 py-4">
+      <div className="container mt-28 lg:mt-24 mx-auto px-12 py-4">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   )
