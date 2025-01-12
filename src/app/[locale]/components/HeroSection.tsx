67f6fa81c54f5643ad12c5bed7900b6235028b12
carlos <c.delriofrances@gmail.com>
2025-01-12 18:28:03 -0300

    removed framer-motion for testing

--- src/app/[locale]/components/HeroSection.tsx
+++ src/app/[locale]/components/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation'
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl'
 
@@ -12,10 +12,10 @@ const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+        <div
+          // initial={{ opacity: 0, scale: 0.5 }}
+          // animate={{ opacity: 1, scale: 1 }}
+          // transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="min-h-[120px] lg:min-h-[450px] text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
@@ -59,11 +59,11 @@ const HeroSection = () => {
               </span>
             </a>
           </div>
-        </motion.div>
-        <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.5 }}
+        </div>
+        <div
+          // initial={{ opacity: 0, scale: 0.5 }}
+          // animate={{ opacity: 1, scale: 1 }}
+          // transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
@@ -75,7 +75,7 @@ const HeroSection = () => {
               height={300}
             />
           </div>
-        </motion.div>
+        </div>
       </div>
     </section>
   )
--- src/app/[locale]/components/ProjectSection.tsx
+++ src/app/[locale]/components/ProjectSection.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef } from 'react'
 import ProjectCard from './ProjectCard'
 import ProjectTag from './ProjectTag'
-import { motion, useInView } from 'framer-motion'
+// import { motion, useInView } from 'framer-motion'
 import { useProjectsData } from '@/config/projectsData'
 import { useTranslations } from 'next-intl'
 
@@ -11,7 +11,7 @@ const ProjectsSection = () => {
 
   const [tag, setTag] = useState('all')
   const ref = useRef(null)
-  const isInView = useInView(ref, { once: true })
+  // const isInView = useInView(ref, { once: true })
   const projectsData = useProjectsData()
 
   const handleTagChange = (newTag: string) => {
@@ -22,10 +22,10 @@ const ProjectsSection = () => {
     project.tag.includes(tag),
   )
 
-  const cardVariants = {
-    initial: { y: 50, opacity: 0 },
-    animate: { y: 0, opacity: 1 },
-  }
+  // const cardVariants = {
+  //   initial: { y: 50, opacity: 0 },
+  //   animate: { y: 0, opacity: 1 },
+  // }
 
   return (
     <section id="projects">
@@ -51,12 +51,12 @@ const ProjectsSection = () => {
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
-          <motion.li
+          <li
             key={index}
-            variants={cardVariants}
-            initial="initial"
-            animate={isInView ? 'animate' : 'initial'}
-            transition={{ duration: 0.3, delay: index * 0.4 }}
+            // variants={cardVariants}
+            // initial="initial"
+            // animate={isInView ? 'animate' : 'initial'}
+            // transition={{ duration: 0.3, delay: index * 0.4 }}
           >
             <ProjectCard
               key={project.id}
@@ -67,7 +67,7 @@ const ProjectsSection = () => {
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
             />
-          </motion.li>
+          </li>
         ))}
       </ul>
     </section>
