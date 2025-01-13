e26bf508ac2a07bd4905742ad7b0f46aa411f33c
carlos <c.delriofrances@gmail.com>
2025-01-13 12:35:30 -0300

    achievements update

--- src/config/achievementsList.tsx
+++ src/config/achievementsList.tsx
@@ -4,9 +4,9 @@ export const useAchievementsList = () => {
   const t = useTranslations('AchievementsSection')
 
   const achievementsList = [
-    { id: 1, metric: t('projects'), value: 30, postfix: '+' },
+    { id: 1, metric: t('projects'), value: 20, postfix: '+' },
     { id: 2, prefix: '~', metric: t('users'), value: 4000 },
-    { id: 3, metric: t('years'), value: 5, postfix: '+' },
+    { id: 3, metric: t('years'), value: 6, postfix: '+' },
   ]
 
   return achievementsList
