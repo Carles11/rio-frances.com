0b3b73d873791071284eaf5d30c9871e10d971c0
carlos <c.delriofrances@gmail.com>
2025-01-13 15:10:39 -0300

    added footer

---
+++ src/app/[locale]/components/Footer.tsx
@@ -0,0 +1,29 @@
+import { useTranslations } from 'next-intl'
+
+const Footer = () => {
+  const t = useTranslations('Footer')
+  const currentYear = new Date().getFullYear()
+  return (
+    <footer className="w-full py-11 mx-auto border border-[#33353F] bottom-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90 text-xs text-white">
+      <div className="container mx-auto text-center">
+        <p>
+          © {currentYear} Carles del Río Francés. {t('rights')}
+        </p>
+        <p>
+          {t('design')}{' '}
+          <a
+            href="https://crix.design"
+            target="_blank"
+            rel="noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            CriX Design
+          </a>
+        </p>
+        <p>{t('no-cookies')}</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,10 +4,11 @@ import ProjectsSection from './components/ProjectSection'
 import EmailSection from '@/app/[locale]/components/EmailSection'
 import AchievementsSection from './components/AchievementsSection'
 import Navbar from '@/app/[locale]/components/Navbar'
+import Footer from '@/app/[locale]/components/Footer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]  ">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-28 lg:mt-24 mx-auto px-8 md:px-12 py-4">
         <HeroSection />
@@ -16,6 +17,7 @@ export default function Home() {
         <ProjectsSection />
         <EmailSection />
       </div>
+      <Footer />
     </main>
   )
 }
