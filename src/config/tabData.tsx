4137314587f8693575f9eca1811c02fc40f3e0e5
carlos <c.delriofrances@gmail.com>
2025-01-13 14:36:43 -0300

    general fixes and updates

--- src/config/tabData.tsx
+++ src/config/tabData.tsx
@@ -9,15 +9,15 @@ export const useTabData = (): TabData[] => {
       title: t('skills.title'),
       id: 'skills',
       content: (
-        <ul className="list-disc pl-2">
-          <li>• {t('skills.node')}</li>
-          <li>• {t('skills.jsTs')}</li>
-          <li>• {t('skills.next')}</li>
-          <li>• {t('skills.react')}</li>
-          <li>• {t('skills.react-native')}</li>
-          <li>• {t('skills.expo')}</li>
-          <li>• {t('skills.mongo')}</li>
-          <li>• {t('skills.supabase')}</li>
+        <ul className="custom-list">
+          <li>{t('skills.node')}</li>
+          <li>{t('skills.jsTs')}</li>
+          <li>{t('skills.next')}</li>
+          <li>{t('skills.react')}</li>
+          <li>{t('skills.react-native')}</li>
+          <li>{t('skills.expo')}</li>
+          <li>{t('skills.mongo')}</li>
+          <li>{t('skills.supabase')}</li>
         </ul>
       ),
     },
@@ -25,37 +25,38 @@ export const useTabData = (): TabData[] => {
       title: t('education.title'),
       id: 'education',
       content: (
-        <ul className="list-disc pl-2">
+        <ul className="custom-list">
           <li>
-            • {t('education.fullStack')}{' '}
+            {t('education.fullStack')}{' '}
             <a
               href="https://www.barcelonacodeschool.com/"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
               {' '}
               @BARCELONA-CODESCHOOL
             </a>
           </li>
           <li>
-            • {t('education.advancedJs')}{' '}
+            {t('education.advancedJs')}{' '}
             <a
               href="https://www.campusmvp.es"
               target="_blank"
               rel="noreferrer"
-              className="hover:underline hove:text-blue-400"
+              className="custom-link"
             >
               {' '}
               @Campus-MVP online school
             </a>
           </li>
           <li>
-            • {t('education.seniorWeb')}{' '}
+            {t('education.seniorWeb')}{' '}
             <a
               href="https://frontendmasters.com/u/elCarles/"
               target="_blank"
               rel="noreferrer"
-              className="hover:underline hove:text-blue-400"
+              className="custom-link"
             >
               {' '}
               @frontendmasters.com
@@ -68,14 +69,15 @@ export const useTabData = (): TabData[] => {
       title: t('certifications.title'),
       id: 'certifications',
       content: (
-        <ul className="list-disc pl-2">
+        <ul className="custom-list">
           <li>
             <a
               href="https://drive.google.com/file/d/1mkz53P0CT9hSnLGQ9YlclDcxe1KH1e3o/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
-              • {t('certifications.fullStack')}
+              {t('certifications.fullStack')}
             </a>
           </li>
           <li>
@@ -83,8 +85,9 @@ export const useTabData = (): TabData[] => {
               href="https://www.campusmvp.es/certificados/carlos-del-rio-frances"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
-              • {t('certifications.advancedJs')}
+              {t('certifications.advancedJs')}
             </a>
           </li>
           <li>
@@ -92,8 +95,9 @@ export const useTabData = (): TabData[] => {
               href="https://university.atlassian.com/student/award/MHMuAWCmDU6kLmPwDyBYge75"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
-              • {t('certifications.jira')}
+              {t('certifications.jira')}
             </a>
           </li>
           <li>
@@ -101,8 +105,9 @@ export const useTabData = (): TabData[] => {
               href="https://university.atlassian.com/student/award/DV7LGD4bsZ5K83ehwetb3jTp"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
-              • {t('certifications.confluence')}
+              {t('certifications.confluence')}
             </a>
           </li>
         </ul>
@@ -112,22 +117,24 @@ export const useTabData = (): TabData[] => {
       title: t('languages.title'),
       id: 'languages',
       content: (
-        <ul className="list-disc pl-2">
-          <li>• {t('languages.spanish')}</li>
-          <li>• {t('languages.catalan')}</li>
+        <ul className="custom-list">
+          <li>{t('languages.spanish')}</li>
+          <li>{t('languages.catalan')}</li>
           <li>
+            {t('languages.german')}{' '}
             <a
               href="https://drive.google.com/file/d/11GEuB08Feu198P2-xyUn-CDxLq_q73wj/view?usp=sharing"
               target="_blank"
               rel="noreferrer"
+              className="custom-link"
             >
-              • {t('languages.german')}
-            </a>
+              (KDS)
+            </a>{' '}
           </li>
-          <li>• {t('languages.english')}</li>
-          <li>• {t('languages.french')}</li>
-          <li>• {t('languages.italian')}</li>
-          <li>• {t('languages.portuguese')}</li>
+          <li>{t('languages.english')}</li>
+          <li>{t('languages.french')}</li>
+          <li>{t('languages.italian')}</li>
+          <li>{t('languages.portuguese')}</li>
         </ul>
       ),
     },
